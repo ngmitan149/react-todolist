650937c859bcb2026012f137a204aa5f86730ed5
Tan PC <ngmitan149@gmail.com>
2020-08-21 21:33:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const todos = [
+    'Go to market',
+    'Buy food',
+    'Make dinner'
+  ];
+  let todoElements = todos.map(ele => <li>{ele}</li>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todoElements}
+      </ul>
     </div>
   );
 }
